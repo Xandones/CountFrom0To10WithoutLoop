fb87013cc6993dbf0245e0e8c640e987413b1a72
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-06-03 17:25:08 -0300

    Add project files.

---
+++ CountFrom0To10WithoutLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "0\n";
+	std::cout << "1\n";
+	std::cout << "2\n";
+	std::cout << "3\n";
+	std::cout << "4\n";
+	std::cout << "5\n";
+	std::cout << "6\n";
+	std::cout << "7\n";
+	std::cout << "8\n";
+	std::cout << "9\n";
+	std::cout << "10\n";
+	system("PAUSE");
+}
